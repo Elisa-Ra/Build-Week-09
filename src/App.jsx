30cf00f98607e6ec95823154169f47b3ed27a8cf
Elisabetta Piacquadio <betta.pcq@gmail.com>
2025-12-09 13:09:32 +0100

    create navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import "./App.css"
-import "bootstrap/dist/css/bootstrap.min.css"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SuperiorNavBar from './components/SuperiorNavBar';
 
 function App() {
-  return <></>
+  return (
+    <>
+      <SuperiorNavBar />
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/SuperiorNavBar.jsx
@@ -0,0 +1,127 @@
+import { Container, InputGroup } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Image } from 'react-bootstrap';
+import { FaLinkedin } from 'react-icons/fa';
+import { AiFillHome } from 'react-icons/ai';
+import { IoSearch } from 'react-icons/io5';
+import { HiUsers } from 'react-icons/hi2';
+import { BsBriefcaseFill } from 'react-icons/bs';
+import { BsBellFill, BsChatDotsFill } from 'react-icons/bs';
+import { TfiLayoutGrid3Alt } from 'react-icons/tfi';
+import { TbDots } from 'react-icons/tb';
+import InputGroupText from 'react-bootstrap/esm/InputGroupText';
+
+const SuperiorNavBar = () => {
+  return (
+    <Navbar expand="lg">
+      <Container fluid className="justify-content-between">
+        <Navbar.Brand href="#">
+          <FaLinkedin fill="#0A65C0" />
+        </Navbar.Brand>
+        <Navbar.Toggle className="border-0 ps-0">
+          <IoSearch />
+        </Navbar.Toggle>
+
+        <InputGroup className="w-25 d-none d-lg-flex">
+          <InputGroup.Text className="bg-white border-1 rounded-start-pill">
+            <IoSearch />
+          </InputGroup.Text>
+          <Form.Control
+            type="search"
+            placeholder="Cerca"
+            className="border-start-0 rounded-end-pill form-control-sm"
+            aria-label="Search"
+          />
+        </InputGroup>
+        <div className="d-flex flex-nowrap justify-content-evenly flex-grow-1">
+          <Nav.Link className="text-center">
+            <AiFillHome />
+            <p className="d-none d-lg-flex fs-7">Home</p>
+          </Nav.Link>
+
+          <Nav.Link className="text-center">
+            <HiUsers />
+            <p className="d-none d-lg-flex fs-7">Rete</p>
+          </Nav.Link>
+          <Nav.Link className="text-center">
+            <BsBriefcaseFill />
+            <p className="d-none d-lg-flex fs-7">Lavoro</p>
+          </Nav.Link>
+          <Nav.Link className="text-center">
+            <BsChatDotsFill />
+            <p className="d-none d-lg-flex fs-7">Messaggistica</p>
+          </Nav.Link>
+          <Nav.Link className="text-center">
+            <BsBellFill />
+            <p className="d-none d-lg-flex fs-7">Notifiche</p>
+          </Nav.Link>
+          <Nav.Item>
+            <Navbar.Collapse>
+              <Nav className=" my-lg-0 flex-column justify-content-center align-items-center">
+                <Image
+                  src="https://placebear.com/200/200"
+                  className="profile-img-icon"
+                  roundedCircle
+                />
+                <NavDropdown
+                  title="Tu"
+                  id="navbarScrollingDropdown"
+                  className="fs-7"
+                >
+                  <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">
+                    Another action
+                  </NavDropdown.Item>
+
+                  <NavDropdown.Item href="#action5">
+                    Something else here
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Nav.Item>
+        </div>
+        <Navbar.Toggle
+          aria-controls="navbarScroll"
+          className="border-0 justify-content-center align-items-center"
+        >
+          <TbDots />
+        </Navbar.Toggle>
+        <Navbar.Collapse className="flex-grow-0">
+          <Nav
+            className="me-auto my-2 my-lg-0 flex-column justify-content-center align-items-center border-start"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <TfiLayoutGrid3Alt />
+
+            <NavDropdown
+              title="Per le aziende"
+              id="navbarScrollingDropdown"
+              className="fs-7"
+            >
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Another action
+              </NavDropdown.Item>
+
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav.Link>
+            <p className="premium-link w-50 fs-7 text-decoration-underline">
+              Riattiva Premium con il 50% di sconto
+            </p>
+          </Nav.Link>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default SuperiorNavBar;
