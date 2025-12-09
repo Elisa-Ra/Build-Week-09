e9f53a7d2bb4f28598b8d8e2a14b27e795e8bddb
Elisa Raeli <79461498+Elisa-Ra@users.noreply.github.com>
2025-12-09 12:56:17 +0100

    info

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <>
       {/* Sezione Hero */}
-      <Container className="mx-2 mb-5 mt-5 rounded bg-white p-0 w-100 w-lg-50">
+      <Container className="mx-2 mb-5 mt-5 rounded bg-white p-0 w-100 w-lg-50 border border-1 border-secondary-subtle">
         {/* Immagine di copertina */}
         <Row>
           <Col>
