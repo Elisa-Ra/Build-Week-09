a39359f03ad7b31ee8cbc4957f152a9a6963e86b
Elisa Raeli <79461498+Elisa-Ra@users.noreply.github.com>
2025-12-09 17:40:33 +0100

    margin fix

--- src/components/EducationSection.jsx
+++ src/components/EducationSection.jsx
@@ -1,19 +1,21 @@
-import { Card, Button } from "react-bootstrap"
+import { Container, Card, Button } from "react-bootstrap"
 import EducationCard from "./EducationCard"
 
 const EducationSection = () => {
   return (
-    <Card className="p-3 mb-3 border-0 shadow-sm rounded-3">
-      <h4 className="fw-bold">Formazione</h4>
-      {/* singola formazione */}
-      <EducationCard />
-      <hr />
-      <EducationCard />
-      <hr />
-      <Button className="text-decoration-none text-black bg-transparent border-0 fw-semibold text-primary">
-        Mostra tutti i titoli di studio (4) →
-      </Button>
-    </Card>
+    <Container className=" mx-2 mb-5 mt-0 rounded bg-white p-0 w-100 w-lg-50 border border-1 border-secondary-subtle">
+      <Card className="p-3 mb-3 border-0 shadow-sm rounded-3">
+        <h4 className="fw-bold">Formazione</h4>
+        {/* singola formazione */}
+        <EducationCard />
+        <hr />
+        <EducationCard />
+        <hr />
+        <Button className="text-decoration-none text-black bg-transparent border-0 fw-semibold text-primary">
+          Mostra tutti i titoli di studio (4) →
+        </Button>
+      </Card>
+    </Container>
   )
 }
 
--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -1,18 +1,20 @@
-import { Card, Button } from "react-bootstrap"
+import { Container, Card, Button } from "react-bootstrap"
 import SkillCard from "./SkillCard"
 
 const SkillsSection = () => {
   return (
-    <Card className="p-3 mb-3 border-0 shadow-sm rounded-3">
-      <h4 className="fw-bold">Competenze</h4>
+    <Container className=" mx-2 mb-5 mt-0 rounded bg-white p-0 w-100 w-lg-50 border border-1 border-secondary-subtle">
+      <Card className="p-3 mb-3 border-0 shadow-sm rounded-3">
+        <h4 className="fw-bold">Competenze</h4>
 
-      <SkillCard />
-      <SkillCard />
+        <SkillCard />
+        <SkillCard />
 
-      <Button className="text-decoration-none text-black bg-transparent border-0 fw-semibold text-primary">
-        Mostra tutte le competenze (19) →
-      </Button>
-    </Card>
+        <Button className="text-decoration-none text-black bg-transparent border-0 fw-semibold text-primary">
+          Mostra tutte le competenze (19) →
+        </Button>
+      </Card>
+    </Container>
   )
 }
 
