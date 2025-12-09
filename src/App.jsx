fa1dabffa1e49c4a2f927603ffa46167583b41fa
NAD3R <naderdeghaili@gmail.com>
2025-12-09 13:26:19 +0100

    fixed App

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import SezioneAnalisi from "./components/SezioneAnalisi"
 import RightAside from "./components/RightAside"
 import { Col, Container, Row } from "react-bootstrap"
-import SezioneAnalisi from "./components/SezioneAnalisi"
 import SezioneAttivita from "./components/SezioneAttivita"
 import Information from "./components/Information"
 import MyFooter from "./components/MyFooter"
@@ -14,25 +13,28 @@ import ExperienceSection from "./components/ExperienceSection"
 import EducationSection from "./components/EducationSection"
 
 function App() {
-  return <>(
-    <Container>
-      <Row>
-
-        <Col xs={8}>
-          <Hero />
-          <SezioneAnalisi />
-          <SezioneAttivita />
-          <Information />
-          <ExperienceSection />
-          <EducationSection />
-        </Col>
-        <Col lg={4} xs={12}>
-          <RightAside />
-        </Col>
-      </Row>
-      <MyFooter />
-    </Container>
-  )</>
+  return (
+    <>
+      (
+      <Container>
+        <Row>
+          <Col xs={8}>
+            <Hero />
+            <SezioneAnalisi />
+            <SezioneAttivita />
+            <Information />
+            <ExperienceSection />
+            <EducationSection />
+          </Col>
+          <Col lg={4} xs={12}>
+            <RightAside />
+          </Col>
+        </Row>
+        <MyFooter />
+      </Container>
+      )
+    </>
+  )
 }
 
 export default App
