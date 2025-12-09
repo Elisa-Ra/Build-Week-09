0a2a93186dc3cdec27dad462cddf6df88c12fd8d
Elisa Raeli <79461498+Elisa-Ra@users.noreply.github.com>
2025-12-09 10:31:22 +0100

    Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import "./App.css"
 import "bootstrap/dist/css/bootstrap.min.css"
+import Hero from "./components/Hero"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
-  return <></>
+  return (
+    <>
+      <Hero />
+    </>
+  )
 }
 
 export default App
---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import { Container, Row, Col } from "react-bootstrap"
+
+const Hero = () => {
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col>Sopra</Col>
+          <Col>sotto</Col>
+        </Row>
+      </Container>
+    </>
+  )
+}
+
+export default Hero
