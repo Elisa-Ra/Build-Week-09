717f04191198ca486396ab7614a249bebcaa976e
Salvatore <pepesalvatore12@gmail.com>
2025-12-10 14:48:37 +0100

    Update ProfileSections.jsx

--- src/components/ProfileSections.jsx
+++ src/components/ProfileSections.jsx
@@ -1,16 +1,16 @@
-import { Col, Container, Row } from 'react-bootstrap';
-import EducationSection from './EducationSection';
-import HeroDinamic from './HeroDinamic';
-import LicensesSection from './LicensesSection';
-import SezioneAnalisi from './SezioneAnalisi';
-import SezioneLingue from './SezioneLingue';
-import SkillsSection from './SkillsSection';
-import SezioneAttivita from './SezioneAttivita';
-import ExperienceSection from './ExperienceSection';
-import Interests from './Interests';
-import Information from './Information';
-import RightAside from './RightAside';
-import MyFooter from './MyFooter';
+import { Col, Container, Row } from "react-bootstrap"
+import EducationSection from "./EducationSection"
+import HeroDinamic from "./HeroDinamic"
+import LicensesSection from "./LicensesSection"
+import SezioneAnalisi from "./SezioneAnalisi"
+import SezioneLingue from "./SezioneLingue"
+import SkillsSection from "./SkillsSection"
+import SezioneAttivita from "./SezioneAttivita"
+import ExperienceSection from "./ExperienceSection"
+import Interests from "./Interests"
+import Information from "./Information"
+import RightAside from "./RightAside"
+import MyFooter from "./MyFooter"
 
 const ProfileSections = () => {
   return (
@@ -36,6 +36,6 @@ const ProfileSections = () => {
         <MyFooter />
       </Container>
     </>
-  );
-};
-export default ProfileSections;
+  )
+}
+export default ProfileSections
