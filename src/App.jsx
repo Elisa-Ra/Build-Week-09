61fbd63d16d895e0a1da95a229a9cfcd86a4b0e6
NAD3R <naderdeghaili@gmail.com>
2025-12-10 09:51:50 +0100

    changed App

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,9 @@ import "./App.css"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-icons/font/bootstrap-icons.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import SezioneAnalisi from "./components/SezioneAnalisi"
-import RightAside from "./components/RightAside"
 import { Col, Container, Row } from "react-bootstrap"
-import SezioneAttivita from "./components/SezioneAttivita"
-import Information from "./components/Information"
 import MyFooter from "./components/MyFooter"
 import SuperiorNavBar from "./components/SuperiorNavBar"
-import Interests from "./components/Interests"
-import SezioneLingue from "./components/SezioneLingue"
-import HeroDinamic from "./components/HeroDinamic"
 import SezioneMessaggistica from "./components/SezioneMessaggistica"
 import ProfileSections from "./components/ProfileSections"
 
@@ -20,20 +13,7 @@ function App() {
     <>
       <Container>
         <SuperiorNavBar />
-        <Row className="mt-6">
-          <Col xs={8}>
-            <HeroDinamic />
-            <SezioneAnalisi />
-            <Information />
-            <SezioneAttivita />
-            <SezioneLingue />
-            <ProfileSections />
-            <Interests />
-          </Col>
-          <Col lg={4} xs={12}>
-            <RightAside />
-          </Col>
-        </Row>
+        <ProfileSections />
         <MyFooter />
       </Container>
 
--- src/components/ProfileSections.jsx
+++ src/components/ProfileSections.jsx
@@ -1,15 +1,36 @@
+import { Col, Row } from "react-bootstrap"
 import EducationSection from "./EducationSection"
-import ExperienceSection from "./ExperienceSection"
+import HeroDinamic from "./HeroDinamic"
 import LicensesSection from "./LicensesSection"
+import SezioneAnalisi from "./SezioneAnalisi"
+import SezioneLingue from "./SezioneLingue"
 import SkillsSection from "./SkillsSection"
+import SezioneAttivita from "./SezioneAttivita"
+import ExperienceSection from "./ExperienceSection"
+import Interests from "./Interests"
+import Information from "./Information"
+import RightAside from "./RightAside"
 
 const ProfileSections = () => {
   return (
     <>
-      <ExperienceSection />
-      <EducationSection />
-      <SkillsSection />
-      <LicensesSection />
+      <Row className="mt-6">
+        <Col xs={8}>
+          <HeroDinamic />
+          <SezioneAnalisi />
+          <Information />
+          <SezioneAttivita />
+          <SezioneLingue />
+          <ExperienceSection />
+          <EducationSection />
+          <SkillsSection />
+          <LicensesSection />
+          <Interests />
+        </Col>
+        <Col lg={4} xs={12}>
+          <RightAside />
+        </Col>
+      </Row>
     </>
   )
 }
