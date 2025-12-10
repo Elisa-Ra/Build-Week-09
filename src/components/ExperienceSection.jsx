91dfa3dd314154425dc592810bfd122873df448c
Elisa Raeli <79461498+Elisa-Ra@users.noreply.github.com>
2025-12-10 16:44:15 +0100

    experiencesss

--- src/components/ExperienceSection.jsx
+++ src/components/ExperienceSection.jsx
@@ -1,11 +1,20 @@
-import { Container, Card, Button } from "react-bootstrap"
+import { Container, Card, Button, Row, Col } from "react-bootstrap"
 import ExperienceCard from "./ExperienceCard"
+import FormExperiences from "./FormExperiences"
 
 const ExperienceSection = () => {
   return (
     <Container className=" mx-2 mb-5 mt-0 rounded bg-white p-0 w-100 w-lg-50 border border-1 border-secondary-subtle">
       <Card className="p-3 mb-3 border-0 shadow-sm rounded-3">
-        <h4 className="fw-bold">Esperienza</h4>
+        <Row>
+          <Col>
+            <h4 className="fw-bold">Esperienza</h4>
+          </Col>
+          <Col>
+            <FormExperiences />
+          </Col>
+        </Row>
+
         {/* singola esperienza */}
         <ExperienceCard />
         <hr />
