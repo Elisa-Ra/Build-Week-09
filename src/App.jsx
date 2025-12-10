cf2a2302b3ad3cfe48e0255077d73367e26f7b32
NAD3R <naderdeghaili@gmail.com>
2025-12-10 09:34:50 +0100

    optimized components

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,12 @@ import { Col, Container, Row } from "react-bootstrap"
 import SezioneAttivita from "./components/SezioneAttivita"
 import Information from "./components/Information"
 import MyFooter from "./components/MyFooter"
-import ExperienceSection from "./components/ExperienceSection"
-import EducationSection from "./components/EducationSection"
 import SuperiorNavBar from "./components/SuperiorNavBar"
-import SkillsSection from "./components/SkillsSection"
 import Interests from "./components/Interests"
-import LicensesSection from "./components/LicensesSection"
 import SezioneLingue from "./components/SezioneLingue"
 import HeroDinamic from "./components/HeroDinamic"
 import SezioneMessaggistica from "./components/SezioneMessaggistica"
+import ProfileSections from "./components/ProfileSections"
 
 function App() {
   return (
@@ -30,11 +27,7 @@ function App() {
             <Information />
             <SezioneAttivita />
             <SezioneLingue />
-
-            <ExperienceSection />
-            <EducationSection />
-            <SkillsSection />
-            <LicensesSection />
+            <ProfileSections />
             <Interests />
           </Col>
           <Col lg={4} xs={12}>
---
+++ src/components/ProfileSections.jsx
@@ -0,0 +1,16 @@
+import EducationSection from "./EducationSection"
+import ExperienceSection from "./ExperienceSection"
+import LicensesSection from "./LicensesSection"
+import SkillsSection from "./SkillsSection"
+
+const ProfileSections = () => {
+  return (
+    <>
+      <ExperienceSection />
+      <EducationSection />
+      <SkillsSection />
+      <LicensesSection />
+    </>
+  )
+}
+export default ProfileSections
