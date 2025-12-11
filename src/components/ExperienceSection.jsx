e373a295d8f6807cfa54a70d421e56ec34a581c2
Elisabetta Piacquadio <betta.pcq@gmail.com>
2025-12-11 10:53:47 +0100

    add exp ternary

--- src/components/ExperienceSection.jsx
+++ src/components/ExperienceSection.jsx
@@ -1,25 +1,23 @@
-import ExperienceCard from "./ExperienceCard"
-import { useDispatch, useSelector } from "react-redux"
-import { useEffect } from "react"
-import { getExpAction } from "../redux/actions/experiencesAction"
-import { Container, Card, Button, Row, Col, Spinner } from "react-bootstrap"
+import ExperienceCard from './ExperienceCard';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { getExpAction } from '../redux/actions/experiencesAction';
+import { Container, Card, Button, Row, Col, Spinner } from 'react-bootstrap';
 
-import FormExperiences from "./FormExperiences"
-
+import FormExperiences from './FormExperiences';
 
 const ExperienceSection = (props) => {
-
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const data = useSelector((state) => {
-    return state.experiences.data
-  })
+    return state.experiences.data;
+  });
   const loading = useSelector((state) => {
-    return state.experiences.loading
-  })
+    return state.experiences.loading;
+  });
   useEffect(() => {
-    dispatch(getExpAction(props.ID))
+    dispatch(getExpAction(props.ID));
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [props.ID])
+  }, [props.ID]);
   return (
     <Container className=" mx-2 mb-5 mt-0 rounded bg-white p-0 w-100 w-lg-50 border border-1 border-secondary-subtle">
       <Card className="p-3 mb-3 border-0 shadow-sm rounded-3">
@@ -41,32 +39,37 @@ const ExperienceSection = (props) => {
             )}
 
             {/* map esperienza */}
-            {data &&
-              data.map((exp, index) => {
-                return (
-                  <ExperienceCard
-                    img={exp.image}
-                    role={exp.role}
-                    company={exp.company}
-                    startD={exp.startDate}
-                    endD={exp.endDate}
-                    area={exp.area}
-                    key={index}
-                    description={exp.description}
-                  />
-                )
-              })}
+            {data && data.length > 0 ? (
+              data.map((exp, index) => (
+                <ExperienceCard
+                  img={exp.image}
+                  role={exp.role}
+                  company={exp.company}
+                  startD={exp.startDate}
+                  endD={exp.endDate}
+                  area={exp.area}
+                  key={index}
+                  description={exp.description}
+                />
+              ))
+            ) : (
+              <>
+                <h3 className="fs-5 fw-bold mb-1">
+                  Non hai ancora inserito esperienze
+                </h3>
+                <p className="text-muted small">
+                  Le esperienze che condividi appariranno qui
+                </p>
+              </>
+            )}
           </Col>
         </Row>
 
         {/* singola esperienza */}
-
-        <Button className="text-decoration-none text-black bg-transparent border-0 fw-semibold text-primary">
-          Mostra tutte le esperienze (7) →
-        </Button>
+        {}
       </Card>
     </Container>
-  )
-}
+  );
+};
 
-export default ExperienceSection
+export default ExperienceSection;
--- src/redux/actions/experiencesAction.js
+++ src/redux/actions/experiencesAction.js
@@ -1,141 +1,141 @@
-export const GET_EXP_REQUEST = "GET_EXP_REQUEST"
-export const GET_EXP_SUCCESS = "GET_EXP_SUCCESS"
-export const GET_EXP_ERROR = "GET_EXP_ERROR"
+export const GET_EXP_REQUEST = 'GET_EXP_REQUEST';
+export const GET_EXP_SUCCESS = 'GET_EXP_SUCCESS';
+export const GET_EXP_ERROR = 'GET_EXP_ERROR';
 
-export const PUT_EXP_REQUEST = "PUT_EXP_REQUEST"
-export const PUT_EXP_SUCCESS = "PUT_EXP_SUCCESS"
-export const PUT_EXP_ERROR = "PUT_EXP_ERROR"
+export const PUT_EXP_REQUEST = 'PUT_EXP_REQUEST';
+export const PUT_EXP_SUCCESS = 'PUT_EXP_SUCCESS';
+export const PUT_EXP_ERROR = 'PUT_EXP_ERROR';
 
-export const POST_EXP_REQUEST = "POST_EXP_REQUEST"
-export const POST_EXP_SUCCESS = "POST_EXP_SUCCESS"
-export const POST_EXP_ERROR = "POST_EXP_ERROR"
+export const POST_EXP_REQUEST = 'POST_EXP_REQUEST';
+export const POST_EXP_SUCCESS = 'POST_EXP_SUCCESS';
+export const POST_EXP_ERROR = 'POST_EXP_ERROR';
 
-export const DELETE_EXP_REQUEST = "DELETE_EXP_REQUEST"
-export const DELETE_EXP_SUCCESS = "DELETE_EXP_SUCCESS"
-export const DELETE_EXP_ERROR = "DELETE_EXP_ERROR"
+export const DELETE_EXP_REQUEST = 'DELETE_EXP_REQUEST';
+export const DELETE_EXP_SUCCESS = 'DELETE_EXP_SUCCESS';
+export const DELETE_EXP_ERROR = 'DELETE_EXP_ERROR';
 
-const API_USER = "https://striveschool-api.herokuapp.com/api/profile/"
+const API_USER = 'https://striveschool-api.herokuapp.com/api/profile/';
 const TOKEN =
-  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OTM3ZGIwN2QzMjJmNTAwMTUxMDc2YTAiLCJpYXQiOjE3NjUyNjgyMzEsImV4cCI6MTc2NjQ3NzgzMX0.xpmE3XKHeFyn9woNxPxv0Fs_cK7s5T7gtcpLwDMBGII"
+  'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OTM3ZGIwN2QzMjJmNTAwMTUxMDc2YTAiLCJpYXQiOjE3NjUyNjgyMzEsImV4cCI6MTc2NjQ3NzgzMX0.xpmE3XKHeFyn9woNxPxv0Fs_cK7s5T7gtcpLwDMBGII';
 
 //   GET
 export const getExpAction = (userId) => {
   return (dispatch) => {
-    dispatch({ type: GET_EXP_REQUEST })
-    fetch(`${API_USER}${userId}/experiences`, {
+    dispatch({ type: GET_EXP_REQUEST });
+    fetch(`${API_USER}6937db07d322f500151076a0/experiences`, {
       headers: {
-        Authorization: TOKEN,
-      },
+        Authorization: TOKEN
+      }
     })
       .then((res) => {
         if (res.ok) {
-          return res.json()
-        } else throw new Error("Errore Fetch Profilo" + res.status)
+          return res.json();
+        } else throw new Error('Errore Fetch Profilo' + res.status);
       })
       .then((data) => {
-        dispatch({ type: GET_EXP_SUCCESS, payload: data })
-        console.log(data)
+        dispatch({ type: GET_EXP_SUCCESS, payload: data });
+        console.log(data);
       })
       .catch((err) => {
-        console.log("Errore nel fetch" + typeof err)
+        console.log('Errore nel fetch' + typeof err);
         dispatch({
           type: GET_EXP_ERROR,
-          payload: JSON.stringify(err, Object.getOwnPropertyNames(err)),
-        })
-      })
-  }
-}
+          payload: JSON.stringify(err, Object.getOwnPropertyNames(err))
+        });
+      });
+  };
+};
 
 // PUT
 export const putExpAction = (userId, expId, exp) => {
   return (dispatch) => {
-    dispatch({ type: PUT_EXP_REQUEST })
+    dispatch({ type: PUT_EXP_REQUEST });
     fetch(`${API_USER}${userId}/experiences/${expId}`, {
-      method: "PUT",
+      method: 'PUT',
       headers: {
         Authorization: TOKEN,
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json'
       },
-      body: JSON.stringify(exp),
+      body: JSON.stringify(exp)
     })
       .then((res) => {
         if (res.ok) {
-          return res.json()
-        } else throw new Error("Errore Fetch Profilo" + res.status)
+          return res.json();
+        } else throw new Error('Errore Fetch Profilo' + res.status);
       })
       .then((modifiedData) => {
-        dispatch({ type: PUT_EXP_SUCCESS, payload: modifiedData })
-        console.log(modifiedData)
+        dispatch({ type: PUT_EXP_SUCCESS, payload: modifiedData });
+        console.log(modifiedData);
       })
       .catch((err) => {
-        console.log("Errore nel fetch" + typeof err)
+        console.log('Errore nel fetch' + typeof err);
         dispatch({
           type: PUT_EXP_ERROR,
-          payload: JSON.stringify(err, Object.getOwnPropertyNames(err)),
-        })
-      })
-  }
-}
+          payload: JSON.stringify(err, Object.getOwnPropertyNames(err))
+        });
+      });
+  };
+};
 
 // POST
 
 export const postExpAction = (userId, exp) => {
   return (dispatch) => {
-    dispatch({ type: POST_EXP_REQUEST })
+    dispatch({ type: POST_EXP_REQUEST });
     fetch(`${API_USER}${userId}/experiences`, {
-      method: "POST",
+      method: 'POST',
       headers: {
         Authorization: TOKEN,
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json'
       },
-      body: JSON.stringify(exp),
+      body: JSON.stringify(exp)
     })
       .then((res) => {
         if (res.ok) {
-          return res.json()
-        } else throw new Error("Errore Fetch Profilo", res.status)
+          return res.json();
+        } else throw new Error('Errore Fetch Profilo', res.status);
       })
       .then((postedData) => {
-        dispatch({ type: POST_EXP_SUCCESS, payload: postedData })
-        console.log(postedData)
+        dispatch({ type: POST_EXP_SUCCESS, payload: postedData });
+        console.log(postedData);
       })
       .catch((err) => {
-        console.log("Errore nel fetch" + typeof err)
+        console.log('Errore nel fetch' + typeof err);
         dispatch({
           type: POST_EXP_ERROR,
-          payload: JSON.stringify(err, Object.getOwnPropertyNames(err)),
-        })
-      })
-  }
-}
+          payload: JSON.stringify(err, Object.getOwnPropertyNames(err))
+        });
+      });
+  };
+};
 
 // DELETE
 
 export const deleteExpAction = (userId, expId) => {
   return (dispatch) => {
-    dispatch({ type: DELETE_EXP_REQUEST })
+    dispatch({ type: DELETE_EXP_REQUEST });
     fetch(`${API_USER}${userId}/experiences/${expId}`, {
-      method: "DELETE",
+      method: 'DELETE',
       headers: {
         Authorization: TOKEN,
-        "Content-Type": "application/json",
-      },
+        'Content-Type': 'application/json'
+      }
     })
       .then((res) => {
         if (res.ok) {
-          return res.json()
-        } else throw new Error("Errore Fetch Profilo" + res.status)
+          return res.json();
+        } else throw new Error('Errore Fetch Profilo' + res.status);
       })
       .then((deletedData) => {
-        dispatch({ type: DELETE_EXP_SUCCESS, payload: deletedData })
-        console.log(deletedData)
+        dispatch({ type: DELETE_EXP_SUCCESS, payload: deletedData });
+        console.log(deletedData);
       })
       .catch((err) => {
-        console.log("Errore nel fetch" + typeof err)
+        console.log('Errore nel fetch' + typeof err);
         dispatch({
           type: DELETE_EXP_ERROR,
-          payload: JSON.stringify(err, Object.getOwnPropertyNames(err)),
-        })
-      })
-  }
-}
+          payload: JSON.stringify(err, Object.getOwnPropertyNames(err))
+        });
+      });
+  };
+};
