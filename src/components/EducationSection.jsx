45721b51bd028c1dfdfc8d9b8a467aaaca2b9cfb
Elisabetta Piacquadio <betta.pcq@gmail.com>
2025-12-12 09:48:21 +0100

    fixed shadows

--- src/components/EducationSection.jsx
+++ src/components/EducationSection.jsx
@@ -1,10 +1,10 @@
-import { Container, Card, Button } from "react-bootstrap"
-import EducationCard from "./EducationCard"
+import { Container, Card, Button } from 'react-bootstrap';
+import EducationCard from './EducationCard';
 
 const EducationSection = () => {
   return (
     <Container className=" mx-2 mb-5 mt-0 rounded bg-white p-0 w-100 w-lg-50 border border-1 border-secondary-subtle">
-      <Card className="p-3 mb-3 border-0 shadow-sm rounded-3">
+      <Card className="p-3 mb-3 border-0  rounded-3">
         <h4 className="fw-bold">Formazione</h4>
         {/* singola formazione */}
         <EducationCard />
@@ -16,7 +16,7 @@ const EducationSection = () => {
         </Button>
       </Card>
     </Container>
-  )
-}
+  );
+};
 
-export default EducationSection
+export default EducationSection;
--- src/components/LicensesSection.jsx
+++ src/components/LicensesSection.jsx
@@ -1,33 +1,33 @@
-import { Container, Card } from "react-bootstrap"
-import LicensesCard from "./LicensesCard"
+import { Container, Card } from 'react-bootstrap';
+import LicensesCard from './LicensesCard';
 
 const LicensesSection = () => {
   const certifications = [
     {
-      title: "International English Language Testing System (IELTS) - C1 Level",
-      issuer: "IDP Education Ltd",
-      date: "lug 2025",
-      img: "https://placecats.com/50/50",
-      id: "1",
+      title: 'International English Language Testing System (IELTS) - C1 Level',
+      issuer: 'IDP Education Ltd',
+      date: 'lug 2025',
+      img: 'https://placecats.com/50/50',
+      id: '1'
     },
     {
-      title: "ECDL",
-      issuer: "",
-      date: "gen 2007",
-      img: "https://placecats.com/50/50",
-      id: "2",
-    },
-  ]
+      title: 'ECDL',
+      issuer: '',
+      date: 'gen 2007',
+      img: 'https://placecats.com/50/50',
+      id: '2'
+    }
+  ];
   return (
     <Container className=" mx-2 mb-5 mt-0 rounded bg-white p-0 w-100 w-lg-50 border border-1 border-secondary-subtle">
-      <Card className="p-3 mb-3 border-0 shadow-sm rounded-3">
+      <Card className="p-3 mb-3 border-0 rounded-3">
         <h4 className="fw-bold">Licenze e certificazioni</h4>
         {certifications.map((cert) => {
-          return <LicensesCard key={cert.id} cert={cert} />
+          return <LicensesCard key={cert.id} cert={cert} />;
         })}
       </Card>
     </Container>
-  )
-}
+  );
+};
 
-export default LicensesSection
+export default LicensesSection;
