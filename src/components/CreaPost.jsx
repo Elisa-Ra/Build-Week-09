a39b48fd707a1322f994a751ae0ecd31f8023731
NAD3R <naderdeghaili@gmail.com>
2025-12-12 12:31:15 +0100

    udpated creaPost

--- src/components/CreaPost.jsx
+++ src/components/CreaPost.jsx
@@ -6,21 +6,22 @@ const CreaPost = () => {
   const profile = useSelector((state) => state.profile.data)
   const [showModal, setShowModal] = useState(false)
   return (
-    <div className="card p-3 mb-3">
-      <div className="d-flex align-items-center mb-3">
-        <img
-          src={profile?.image || "https://placebear.com/100/100"}
-          className="bg-secondary rounded-circle me-3"
-          style={{ width: "45px", height: "40px" }}
-        ></img>
-
-        <input
-          type="text"
-          className="form-control rounded-pill"
-          placeholder="Crea un post"
-          style={{ height: "50px" }}
-        />
-      </div>
+    <>
+      <div className="card p-3 mb-3">
+        <div className="d-flex align-items-center mb-3">
+          <img
+            src={profile?.image || "https://placebear.com/100/100"}
+            className="bg-secondary rounded-circle me-3"
+            style={{ width: "45px", height: "40px" }}
+          ></img>
+          <div
+            className="form-control rounded-pill text-start"
+            style={{ height: "50px", cursor: "pointer" }}
+            onClick={() => setShowModal(true)}
+          >
+            Crea un Post
+          </div>
+        </div>
 
         <div className="d-flex justify-content-around pt-2">
           <button
