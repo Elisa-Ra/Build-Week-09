7fd7c89af2beeed3a6c9369b030a09fbcc86a079
Elisa Raeli <79461498+Elisa-Ra@users.noreply.github.com>
2025-12-12 11:50:36 +0100

    posttt

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,40 +1,77 @@
 import { Container, Row, Col, Image } from "react-bootstrap"
-import EditExperienceForm from "./EditExperienceForm"
+// importare i dati del post
 
-const PostCard = (props) => {
+const PostCard = () => {
   return (
-    <Container className="p-2 mb-3">
+    <Container className="p-2 mb-3 bg-white rounded border border-1 border-secondary-subtle">
+      {/* Row con il mini hero dell'utente */}
       <Row>
+        {/*IMMAGINE UTENTE */}
+        {/* Colonna sinistra superiore */}
         <Col xs={2}>
-          {/* Logo azienda */}
           <Image
-            src={props.img}
-            className="rounded overflow-hidden border bg-light"
+            // src={props.img}
+            src="https://placecats.com/50/50"
+            className=" overflow-hidden border bg-light"
             style={{ width: "48px", height: "48px" }}
           ></Image>
         </Col>
-
+        {/* Colonna centrale superiore */}
         <Col xs={8}>
-          <h6 className="fw-bold mb-0">{props.role}</h6>
-          <p className="mb-0  text-muted">{props.company}</p>
-          <p className="mb-0 small text-muted">{/* · 2 anni 3 mesi */}</p>
-          <p className="mb-2 small text-muted">{props.area}</p>
-
-          <p className="small fw-semibold">{props.description}</p>
+          <Col>
+            {/* Aggiungere nome */}
+            <p className="my-0">
+              <strong>Nome</strong>
+            </p>
+          </Col>
+          <Col>
+            <p className="fs-7 text-secondary mb-0">76.559 follower</p>
+          </Col>
+          {/* Aggiungere createdAt */}
+          <Col>
+            <p className="fs-7 text-secondary">
+              createdAt • <i className="bi bi-globe-americas"></i>
+            </p>
+          </Col>
         </Col>
+        {/* Colonna destra superiore */}
         <Col xs={2}>
-          {/* QUA AGGIUNGERE I DATI DEL POST CHE DEVONO APPARIRE */}
-          {/* <EditExperienceForm
-            img={props.image}
-            role={props.role}
-            company={props.company}
-            startD={props.startD}
-            endD={props.endD}
-            area={props.area}
-            description={props.description}
-            expID={props.expID}
-            ID={props.ID}
-          /> */}
+          <i className="bi bi-three-dots"></i> <i class="bi bi-x-lg"></i>
+        </Col>
+      </Row>
+      {/* Row con il TESTO dell'utente */}
+      <Row>
+        <Col>
+          <p>aggiungere testo utente</p>
+        </Col>
+      </Row>
+      {/* La row qua sotto serve per l'immagine se è presente! */}
+      <Row>
+        <Col>
+          {/* <Image
+            // src={props.img}
+            src="https://placecats.com/50/50"
+            className=" overflow-hidden border bg-light"
+            style={{ width: "48px", height: "48px" }}
+          ></Image> */}
+        </Col>{" "}
+      </Row>
+      {/* Row di pulsanti */}
+      <Row>
+        <Col xs={3} className="pe-0">
+          <i className="bi bi-hand-thumbs-up"></i>
+          <p className="d-inline fs-6">Consiglia</p>
+        </Col>
+        <Col xs={3} className="px-0">
+          <i className="bi bi-chat-text"></i>{" "}
+          <p className="d-inline">Commenta</p>
+        </Col>
+        <Col xs={3} className="px-0">
+          <i className="bi bi-arrow-repeat"></i>{" "}
+          <p className="d-inline">Diffondi il post</p>
+        </Col>
+        <Col xs={3} className="pe-0">
+          <i className="bi bi-send-fill"></i> <p className="d-inline">Invia</p>
         </Col>
       </Row>
     </Container>
